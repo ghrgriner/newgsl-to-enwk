cec8e96e892e8456abad003dde5d9eb67e549096
ghrgriner <rgriner_fwd@outlook.com>
2026-09-01 13:48:47 -0500

    1. Create new `../output/intermediate/tr_attrition_md.txt` file
    2. Also add many comments and refactor a little

--- src/run_trans_stats_newgsl.py
+++ src/run_trans_stats_newgsl.py
@@ -20,8 +20,9 @@ table in the wiki.
 item, but the rows of the table are in GitHub markdown format for pasting
 into the wiki.
 
-5. This is not an output file, but the first table in the Results page of
-the wiki is from the last sets of `value_counts` printed to stdout.
+5. TR_ATTR_FILE (not public): attrition table presented at the top of the
+results wiki that shows the counts of records in the source deck that
+were included in the translation completion analysis
 '''
 
 import pandas as pd
@@ -34,7 +35,7 @@ from trans_file_util import get_token2, add_tseq
 #------------------------------------------------------------------------------
 NROWS = None # rows to use from ENWK_TRANS_FILE
 
-ENWK_TRANS_FILE = '../output/intermediate/en_sel_wide_trans.txt'
+ENWK_WIDE_TRANS_FILE = '../output/intermediate/en_sel_wide_trans.txt'
 INPUT_LANG_FILE = '../input/lang_names_to_code.txt'
 NEWGSL_FILE = '../output/deck/newgsl_deck.txt'
 
@@ -52,6 +53,15 @@ for q in ['','_1','_2','_3','_4','_5']:
     TRANS_STATS_VARS.extend([item + q for item in SECT_VARS])
 
 MD_ROW_FILE = '../output/intermediate/tr_stats_newgsl_md.txt'
+
+TR_ATTR_FILE = '../output/intermediate/tr_attrition_md.txt'
+
+TR_ATTR_ROWS = [
+ ('_NOSENSE', 'Page exists but no translation table [a]'),
+ ('_UNEXNM', 'Matching translation entry or entries expected'
+             ' but none available [b]'),
+ ('LINK', 'Matching translation entry or entries available [c]'),
+]
 
 #------------------------------------------------------------------------------
 # Functions
@@ -92,7 +102,7 @@ def calc_freq(group, var):
                      'pct100': pct100, 'pct100str': pct100str})
 
 def get_indices_to_drop(pages_to_keep):
-    page_df = pd.read_csv(ENWK_TRANS_FILE, sep='\t',
+    page_df = pd.read_csv(ENWK_WIDE_TRANS_FILE, sep='\t',
                    quoting=csv.QUOTE_MINIMAL, usecols=['page'], nrows=NROWS,
                    na_filter=False)
     page_df['keep_page'] = page_df.page.map(lambda x: x in pages_to_keep)
@@ -106,25 +116,65 @@ def get_indices_to_drop(pages_to_keep):
                    }
     return skip_indices
 
+def print_attrition(attr_file, vocab_deck, tkdf_, sm_word_ids):
+    # Identify unexnm (unexpected not-matched) words in list. `
+    for_unexnm = tkdf_.merge(sm_word_ids, how='right', on='word_id',
+                             indicator='prob')
+    unexnm = for_unexnm[for_unexnm.prob == 'right_only']
+
+    vocab_deck['avail'] = vocab_deck.enwk_def.map(
+                lambda x: x if x.startswith('_') else 'LINK')
+    vocab_deck.loc[vocab_deck.word_id.isin(unexnm.word_id),'avail'] = '_UNEXNM'
+
+    haslink = vocab_deck[vocab_deck.avail == 'LINK']
+    print(haslink.freq_cat.value_counts())
+    print(haslink.freq_cat.value_counts(normalize=True))
+
+    n = vocab_deck.avail.value_counts()
+    pct = vocab_deck.avail.value_counts(normalize=True)
+    tbl = pd.DataFrame(TR_ATTR_ROWS, columns=['avail','label'])
+    tbl = tbl.set_index('avail')
+    tbl['cnt'] = n
+    tbl['cnt'] = tbl.cnt.fillna(0).astype(int)
+    tbl['proportion'] = pct
+    tbl['proportion'] = tbl.proportion.fillna(0)
+    tbl = tbl.reset_index()
+    rank_dict = { code: idx for idx, (code, _) in enumerate(TR_ATTR_ROWS) }
+    tbl['rank'] = tbl.avail.map(lambda x: rank_dict[x])
+    tbl = tbl.sort_values(['rank'])
+    #tbl['label'] = tbl.avail.map(lambda x: label_dict[x])
+    tbl['pct100str'] = tbl.proportion.map(lambda x: str(round(x*100, 1)))
+    tbl['md_row'] = ('| ' + tbl.label + ' | ' + tbl.cnt.astype(str) +
+                    ' | ' + tbl.pct100str + ' |')
+    tbl[['md_row']].to_csv(attr_file, sep='\t', quoting=csv.QUOTE_NONE,
+                           index=False)
+    print(tbl[['label','cnt','pct100str']])
+
 #------------------------------------------------------------------------------
 # Main Entry Point
 #------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------
+# 1. Get the language file, because we will use the 'wide' translation file
+#    which identifies languages by code but not description.
+#------------------------------------------------------------------------------
 ldf = pd.read_csv(INPUT_LANG_FILE, sep='\t', quoting=csv.QUOTE_NONE,
                   na_filter=False)
-
 LANG_DICT = { cod: dsc for cod, dsc in ldf[['lang_code','lang_desc']].values }
 
-# 1. Get the new-GSL list
-
+#------------------------------------------------------------------------------
+# 2. Get the new-GSL list, which we refer to as the 'source deck'. Rarely we
+# look on more than one page for a source deck entry, so the data frame is
+# 'exploded' on the '|'-delimited tokens in `enwk_page`.
+#------------------------------------------------------------------------------
 df = pd.read_csv(NEWGSL_FILE, sep='\t', quoting=csv.QUOTE_NONE,
                  usecols=['word_id','newgsl_line','sseq','enwk_pos',
                           'enwk_page','enwk_def','newgsl_freq_rank'],
                  na_filter=False)
 df['freq_cat'] = df.newgsl_freq_rank.map(freq_to_cat)
 print(df.freq_cat.value_counts())
-word_ids = df[ df.enwk_def != '_NOSENSE' ][['word_id']]
-df.loc[ df.enwk_def == '_NOSENSE', 'enwk_page' ] = ''
+should_match_word_ids = df[~df.enwk_def.str.startswith('_')][['word_id']]
+df.loc[df.enwk_def.str.startswith('_'), 'enwk_page'] = ''
 
 deck_copy = df.copy()
 df = df.fillna('')
@@ -139,6 +189,11 @@ print(x_df)
 x_df['page'] = x_df.enwk_page_1tok.copy()
 
 #------------------------------------------------------------------------------
+# 3. Read (wide) translation file, keeping only pages identified in the source
+# deck data frame above.
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
 # The wide file has many records we don't need and many variables, so we can
 # save a significant amount of time by reading the file twice. First we read
 # only the `page` column to get the row indices of the transtab entries on
@@ -146,12 +201,11 @@ x_df['page'] = x_df.enwk_page_1tok.copy()
 # but skipping most rows. This reduces runtime from 2.5 to 1 minute (for the
 # whole program). Memory consumption is greatly reduced as well. Before the
 # fix res M was around 13 g and up to 25 g virtual. Now, res M tops around
-# 5 g with negligible virtual use.
+# 5 g with negligible virtual memory use.
 #------------------------------------------------------------------------------
 indices_to_drop = get_indices_to_drop(pages_to_keep=set(x_df.page.tolist()))
 
-# 1. Input translation file
-t_df = pd.read_csv(ENWK_TRANS_FILE, sep='\t', quoting=csv.QUOTE_MINIMAL,
+t_df = pd.read_csv(ENWK_WIDE_TRANS_FILE, sep='\t', quoting=csv.QUOTE_MINIMAL,
                    nrows=NROWS,
                    skiprows=lambda x: x in indices_to_drop,
                    na_filter=False)
@@ -159,8 +213,11 @@ t_df['tt_param1'] = t_df.transtop_line.map(get_token2)
 add_tseq(t_df)
 print(t_df)
 
-# 3. Merge translations and (exploded) deck
-
+#-----------------------------------------------------------------------------
+# 3. Merge translations and (exploded) source deck data frames. Limit to
+# matching part of speech, with all records used if `enwk_part_of_speech` is
+# missing.
+#-----------------------------------------------------------------------------
 tk_df = t_df.merge(
     x_df[['word_id','newgsl_line','sseq','page','enwk_pos','freq_cat',
           'seq_of_ref']],
@@ -171,10 +228,9 @@ print(tk_df)
 tk_df = tk_df[(tk_df.enwk_part_of_speech == '') |
               (tk_df.enwk_part_of_speech == tk_df.enwk_pos)]
 
-for_unexnm = tk_df.merge(word_ids, how='right', on='word_id',
-                         indicator='prob')
-unexnm = for_unexnm[for_unexnm.prob == 'right_only']
-
+#------------------------------------------------------------------------------
+# 5. Transform translation set from wide to long.
+#------------------------------------------------------------------------------
 dupkey(df_=tk_df, vars_=['word_id','newgsl_line','sseq','page','tt_param1',
                         'seq_in_param1','seq_of_ref'])
 
@@ -191,9 +247,14 @@ tk_long.rename(columns = {'tr_enwk_': 'translation'}, inplace=True)
 print('\nPrinting tk_long')
 print(tk_long)
 
-# We already have a wide data frame, but pivoting back only loses
-# a bit of time and the code is cleaner so worth it we think
+tk_long[TRANS_AVAIL_VARS + TRANS_LS_ADDL_VARS].to_csv(
+    TRANS_LANG_SENSE_FILE, sep='\t', quoting=csv.QUOTE_NONE, index=False)
 
+#------------------------------------------------------------------------------
+# 6. We already have a wide data frame, but pivoting back only loses
+# a bit of time and the code is cleaner (compare to commented-out below)
+# so we will use `pivot`.
+#------------------------------------------------------------------------------
 tk_wide = tk_long.pivot(index=['word_id','newgsl_line','sseq','page',
                'enwk_part_of_speech','tt_param1','seq_in_param1','seq_of_ref'],
                         columns = 't_lang',
@@ -224,21 +285,22 @@ tk_wide.to_csv(TRANS_AVAIL_FILE, sep='\t', quoting=csv.QUOTE_NONE)
 #                      index=False)
 #-------------------------------------
 
-# Save the long file
-tk_long[TRANS_AVAIL_VARS + TRANS_LS_ADDL_VARS].to_csv(
-    TRANS_LANG_SENSE_FILE, sep='\t', quoting=csv.QUOTE_NONE, index=False)
-
-# Now, need a data frame, one record per word_id x lang, restricted to
+#---------------------------------------------------------------------------
+# 7. Now, need a data frame, one record per word_id x lang, restricted to
 #   word_ids where `enwk_def` does not start with '_' with indicator whether
-#   all `word_id` records are True
-
+#   `has_trans == True` for any entry. This is the analysis set for the
+#    translation completion percentages.
+#---------------------------------------------------------------------------
 tk_for_summ = tk_long.groupby(
        ['word_id','newgsl_line','sseq','lang','freq_cat'])[
            'has_trans'].agg(any).reset_index()
 print(tk_for_summ)
 
+# overall translation completion stats (num, den, pct [as nbr and str])
 final_df = tk_for_summ.groupby('lang').apply(calc_has_trans_freq,
                                              include_groups=False)
+
+# translation completion stats by frequency quartile
 for q in [1,2,3,4,5]:
     tk_for_summ_subset = tk_for_summ[tk_for_summ.freq_cat == q]
     add_df = tk_for_summ_subset.groupby('lang').apply(calc_has_trans_freq,
@@ -258,11 +320,9 @@ final_df['md_row'] = ('| ' + final_df.lang + ' | ' + final_df.lang_desc +
 final_df['md_row'].to_csv(MD_ROW_FILE, sep='\t',
                           quoting=csv.QUOTE_NONE, index=False)
 
-deck_copy['avail'] = deck_copy.enwk_def.map(
-            lambda x: x if x.startswith('_') else 'LINK')
-deck_copy.loc[deck_copy.word_id.isin(unexnm.word_id), 'avail'] = '_UNEXNM'
-print(deck_copy.avail.value_counts())
-print(deck_copy.avail.value_counts(normalize=True))
-haslink = deck_copy[deck_copy.avail == 'LINK']
-print(haslink.freq_cat.value_counts())
-print(haslink.freq_cat.value_counts(normalize=True))
+#-----------------------------------------------------------------------------
+# 8. 'Attrition' counts for source deck. That is, give number of entries
+# included in translation completion analysis and reason excluded.
+#-----------------------------------------------------------------------------
+print_attrition(attr_file=TR_ATTR_FILE, vocab_deck=deck_copy, tkdf_=tk_df,
+                sm_word_ids=should_match_word_ids)
